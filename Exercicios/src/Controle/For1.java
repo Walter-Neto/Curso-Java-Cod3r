4d22c2973db05e2b8122b8ff9d1dda830962f7d0
Walter Barcellos Pereira Neto <walter.barcellos.pereira.neto@gmail.com>
2022-09-27 16:10:52 -0300

    For

---
+++ Exercicios/src/Controle/For1.java
@@ -0,0 +1,17 @@
+package Controle;
+
+public class For1 {
+
+	
+	public static void main(String[] args) {
+		for(int i = 0; i <= 20; i+= 2) {
+			System.out.println("i = " + i);
+		}
+		
+		System.out.println();
+		for(int a = 0; a <= 10; a++) {
+			System.out.println("a = " + a);
+		}
+	}
+	
+}
