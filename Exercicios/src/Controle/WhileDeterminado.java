00268af5885820c62ba7a0ba064e5851a47d3fdc
Walter Barcellos Pereira Neto <walter.barcellos.pereira.neto@gmail.com>
2022-09-27 11:36:06 -0300

    While-determinado

---
+++ Exercicios/src/Controle/WhileDeterminado.java
@@ -0,0 +1,14 @@
+package Controle;
+
+public class WhileDeterminado {
+	
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		while(i <= 10){
+			System.out.printf("i = %d \n", i);
+			i++;
+		}
+	}
+}
