4730de3df8c6fb0178bf5cdc93a953bb682a034c
Walter Barcellos Pereira Neto <walter.barcellos.pereira.neto@gmail.com>
2022-10-10 18:10:04 -0300

    PrimeiroTrauma

---
+++ Exercicios/src/Classe/PrimeiroTrauma.java
@@ -0,0 +1,21 @@
+package Classe;
+
+public class PrimeiroTrauma {
+	
+	/*
+	 * O desafio consiste em fazer com que as variáveis
+	 * 'a' e 'b' sejam acessadas dentro do método main.
+	 */
+	
+	static int a = 3;
+	int b = 10;
+	
+	public static void main(String[] args) {
+		
+		System.out.println(a);		
+		
+		PrimeiroTrauma b = new PrimeiroTrauma();
+		System.out.println(b);
+	}
+
+}
