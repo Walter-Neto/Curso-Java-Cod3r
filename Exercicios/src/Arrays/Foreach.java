dbaa087c5c3e84d6737d65546def2885325572c8
Walter Barcellos Pereira Neto <walter.barcellos.pereira.neto@gmail.com>
2022-10-18 09:07:58 -0300

    Foreach

---
+++ Exercicios/src/Arrays/Foreach.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class Foreach {
+	
+	public static void main(String[] args) {
+		
+		double notas [] = {9.9, 7.5, 6.6};
+		
+		for(double nota:notas) {
+			System.out.println(nota);
+		}
+	
+	}
+}
+	
