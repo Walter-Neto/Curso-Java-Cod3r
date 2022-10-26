19f960da93bdf97ba630992a448154ddbf7c1fd6
Walter Barcellos Pereira Neto <walter.barcellos.pereira.neto@gmail.com>
2022-10-25 23:39:04 -0300

    oo.compra

---
+++ Exercicios/src/oo/composicao/Motor.java
@@ -0,0 +1,22 @@
+package oo.composicao;
+
+public class Motor {
+	
+	double fatorInjecao = 1;
+	boolean ligado = false;
+	
+	Carro carro;
+	
+	int giros() {
+		
+		if(!ligado) {
+			return 0;
+		}else {
+			return (int) Math.round(fatorInjecao * 3000);			
+		}
+		
+	}
+	
+	
+
+}
